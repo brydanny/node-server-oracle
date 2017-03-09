743dc71fa82110267a99b81d68386f463091f05e
dchambi <dchambi@DAZASOFTWARESA.LOCAL>
2017-03-09 11:23:00 -0400

    rutas
    configuración final de rutas genericas

--- core/router.js
+++ core/router.js
@@ -3,12 +3,3 @@ var express = require('express');
 var router = express.Router();
 router.use('/example', require('../modules/example/exampleRoot'));
 module.exports = router;
-
-/*
-module.exports = function(webServer){
-    const router = webServer.getRouter();
-    router.use('/departament', require('../modules/departament/departamentRoot'));
-    router.use('/employees', require('../modules/employees/employeesRoot'));
-    router.use('/job_history', require('../modules/job_history/job_historyRoot'));
-}
-*/
